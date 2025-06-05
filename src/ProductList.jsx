79b8fe848677ada7f91581a62b14d4dda8486b44
JAKDev-Code <joseph.kevin.a@gmail.com>
2025-06-05 16:42:21 -0400

    updated the handleAddToCart function

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+import { addItem } from './CartSlice';
+
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
+    const [addedToCart, setAddedToCart] = useState({});
 
     const plantsArray = [
         {
@@ -252,6 +255,16 @@ function ProductList({ onHomeClick }) {
         e.preventDefault();
         setShowCart(false);
     };
+
+    const handleAddToCart = (product) => {
+    dispatch(addItem(product)); // Dispatch the action to add the product to the cart (Redux action)
+
+    setAddedToCart((prevState) => ({ // Update the local state to reflect that the product has been added
+        ...prevState, // Spread the previous state to retain existing entries
+        [product.name]: true, // Set the current product's name as a key with value 'true' to mark it as added
+    }));
+    };
+
     return (
         <div>
             <div className="navbar" style={styleObj}>
@@ -290,7 +303,7 @@ function ProductList({ onHomeClick }) {
                             <div className="product-title">{plant.name}</div> {/* Display plant name */}
                             {/* Display other plant details like description and cost */}
                             <div className="product-description">{plant.description}</div> {/* Display plant description */}
-                            <div className="product-cost">${plant.cost}</div> {/* Display plant cost */}
+                            <div className="product-cost">{plant.cost}</div> {/* Display plant cost */}
                             <button
                                 className="product-button"
                                 onClick={() => handleAddToCart(plant)} // Handle adding plant to cart
